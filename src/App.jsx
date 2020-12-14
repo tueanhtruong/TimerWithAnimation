e1d591c14b4d6487777fba8e10dd17c22cdcea9a
Loc Tran <loc_tran@dathouse.com>
2020-12-14 20:04:05 +0700

    initial timer project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+const App = (props) => {
+  return (
+    <div className="app">
+      <h1>Hello, World!</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/component/Button/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './style.css';
+
+const Button: React.FC<Props> = ({
+  isFull,
+  title,
+  variant = 'primary',
+  ...props
+}) => {
+  return (
+    <button
+      className={`button button--${variant} ${isFull ? 'button--full' : ''}`}
+      {...props}
+    >
+      {title}
+    </button>
+  );
+};
+
+type Props = React.DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+> & {
+  isFull: boolean;
+  variant: 'primary' | 'danger' | 'warning';
+};
+
+export default Button;
